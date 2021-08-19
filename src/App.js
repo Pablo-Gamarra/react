22c48af2b45ea0ba26beb769fdeafda74a1f751d
Pablo Gamarra <pgamarra27@gmail.com>
2021-08-19 11:37:00 -0300

    Estructura de Reactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+
+/*import logo from './logo.svg';*/
+import React from 'react';
 import './App.css';
 
+/*function Helloworld(props) {
+  return (
+    <div id="hello">
+      <h3>{props.subtitle}</h3>
+      {props.mytext}
+    </div>
+  )
+}*/
+
+class Helloworld extends React.Component {
+
+  state = {
+    show: true // false
+  } 
+
+  toggleShow = () => {
+    this.setState({show: !this.state.show})
+  }
+
+  render() {
+    if (this.state.show){
+      return (
+        <div id="hello">
+          <h3>{this.props.subtitle}</h3>
+          {this.props.mytext}
+          <button onClick={this.toggleShow}>Toggle Show</button>
+        </div>
+      ) 
+    } else {
+      return <h1> 
+        There are not elements
+        <button onClick={this.toggleShow}>Toggle Show</button>
+        </h1>
+    }
+   
+  }
+}
+
+// Otra forma de escribir lo mismo mediante una función flecha
+//const App = () => <div>This is my component:<Helloworld/><Helloworld/></div>
+
+/* Otra forma de escribir lo mismo mediante una clase
+class App extends React.Component {
+  render() {
+    return  <div>This is my component:<Helloworld/><Helloworld/></div>
+  }
+}
+*/
+// Otra forma de escribir lo mismo mediante una función
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is my component:
+      <Helloworld mytext="Hello Pablo" subtitle="Props one"/>
+      <Helloworld mytext="Hola mundo" subtitle="Dos"/> 
+      <Helloworld mytext="Hello" subtitle="Props three"/>
     </div>
   );
 }
